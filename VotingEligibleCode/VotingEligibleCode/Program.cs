1e9ff0cab0df3ddacf64e625948a3883370eabd5
steverollings67 <steverollings67@gmail.com>
2021-07-24 17:45:35 -0400

    new if statement

--- VotingEligibleCode/VotingEligibleCode/Program.cs
+++ VotingEligibleCode/VotingEligibleCode/Program.cs
@@ -8,6 +8,21 @@ namespace VotingEligibleCode
         {
             Console.WriteLine("What's your name?");
             string userName = Console.ReadLine();
+            Console.WriteLine("Enter your age?");
+            string age = Console.ReadLine();
+            int age2 = Int32.Parse(age);
+            if (age2 >= 18)
+            {
+                Console.WriteLine("Great News! You can vote!");
+            }
+            else if (age2 <= 17)
+            {
+                Console.WriteLine("Sorry. You're too young to vote");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
         }
     }
 }
